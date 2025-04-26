73f5d12f6eacb590a555c0c1fafd9f326985950b
Anshu Kumari <anshumehta2010@gmail.com>
2025-04-26 20:11:27 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Anshu Kumari',
+    author_email = 'anshumehta2010@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
